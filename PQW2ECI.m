2823cf1c937a2093093c14469aa849e44bd94a12
tjtkd <tjtkd@localhost>
2023-05-22 15:40:18 +0900

    perifocal 행렬을 넣는법을 고민중

---
+++ PQW2ECI.m
@@ -0,0 +1,8 @@
+%https://github.com/ssw14/seosnagwon_19011248.git
+%
+function[arg_prg,inc_angle,RAAN]=PQW2ECI(A)
+%perifocal_frame-> eci frame
+%
+arg_prg=input('enter number');
+inc_angle=input('enter number');
+RAAN=input('enter number');
