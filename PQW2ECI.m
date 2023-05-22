aadd873db89ff083b25ee8384f8dc65e719e8b9c
tjtkd <tjtkd@localhost>
2023-05-22 15:54:03 +0900

    과정이 이렇게 되겠구나라고 생각하는데 세부적인걸 모르겠다 시간을 좀 들여야할듯

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -6,3 +6,12 @@ function[arg_prg,inc_angle,RAAN]=PQW2ECI(A)
 arg_prg=input('enter number');
 inc_angle=input('enter number');
 RAAN=input('enter number');
+
+perifocal();
+
+r1=[cos(arg_prg) -sin(arg_prg) 0; sin(arg_prg) cos(arg_prg) 0; 0 0 1;]
+r2=[1 0 0; 0 cos(inc_angle) -sin(inc_angle); 0 sin(inc_angle) cos(inc_angle)]
+r3=[cos(RAAN) -sin(RAAN) 0; sin(RAAN) cos(RAAN) 0; 0 0 1;]
+a=perifocal*r1;
+a=a*r2;
+A=a*r3;
