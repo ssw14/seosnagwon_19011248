99512a0d4661e34b62fc3c00a941248f28c3e7c2
tjtkd <tjtkd@localhost>
2023-05-29 15:46:26 +0900

    k라는 변수가 인식이 안되고 있는 상황 이부분을 해결하면 될거같음

---
+++ work.m
@@ -0,0 +1,21 @@
+
+function [velocityInPQW, rangeInPQW] = work(semimajor_axis, eccentricity, true_anomaly)
+% input, output 의 단위는 km, deg로 통일한다
+mu=3.986*10^14;%[m^3/s^2]
+semimajor_axis=input('enter=')
+eccentricity=input('enter=')
+true_anomaly=input('enter=')
+
+    function r =work2(semimajor_axis, eccentricity, true_anomaly)
+        r=semimajor_axis/(1+eccentricity*cos(true_anomaly));
+    end
+
+    function k= work3(mu,semimajor_axis)
+        k=sqrt(mu/semimajor_axis);
+    end
+
+velocityInPQW = k*[-sin(true_anomaly); eccentricity+cos(true_anomaly); 0];
+rangeInPQW=[r*cos(true_anomaly); r*sin(true_anomaly); 0];
+end
+
+ 
